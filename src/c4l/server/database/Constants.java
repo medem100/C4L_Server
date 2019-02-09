2c01013256324a0b5b1ea2cc528ee5a1e2abe239
Andre Soltau <Modellbauer555@web.de>
2019-02-09 01:09:52 +0100

    an der schnitztelle gebastelt

---
+++ src/c4l/server/database/Constants.java
@@ -0,0 +1,58 @@
+package c4l.server.database;
+
+/*
+ * Constants for the whole project
+ */
+
+public class Constants {
+	//Ports and adrres
+	/** Port, where Arduino of Hardware-Controller is connected */
+	public final static String ARDUINO_PORT = "COM6";
+	
+	/** Addres from the server **/
+	public final static String SERVER_IP = "127.0.0.1";
+    public final static String SERVER_Port = "127.0.0.1";
+    public final static String SERVER_ADDRESS = SERVER_IP+"::"+SERVER_Port;
+    					
+			
+	//General
+	/** Number of channels a device has. */
+	public final static int DEVICE_CHANNELS = 16;
+	/** Number of full-featured devices */
+	public final static int DYNAMIC_DEVICES = 30;
+	/** Number of devices with reduced features */
+	public final static int STATIC_DEVICES = 2; //QUESTION Ist das sinnvoll, dass noch als Device zu handhaben? Würde am Anfang einfach stumpf die Werte durchschleusen 
+												// Habs nur einfach erst mal trenne wollen und es mehr oder weniger so auch dockumentiern
+	/** Total number of Devices */
+	public final static int ALL_DEVICES = DYNAMIC_DEVICES + STATIC_DEVICES;
+	
+	public final static int[] PERMUTATION = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	
+	// Input constants
+	/** Tolerance of variation on the hardware-faders, that are interpreted as side-effects, rather than as inputs. */
+	public final static int FADER_TOLERANCE = 3;
+	/** Correction factor to change from hardware range (0-1023) to DMX-Range (0-255) */
+	public final static int CORRECTIONDIVISOR = 4;
+	/** Range of the rotary value. */
+	public final static int ROTARY_RANGE = 1024; //TODO maybe adjust value
+	
+	
+	// Output constants
+	/** Last Channel for Enttec-Output **/
+	public final static int  MAXCHANNEL = 511;
+	/** First Channel for Enttec-Output */
+	public final static int  MINCHANNEL = 0;
+	/** Maximum output-value for DMX-protocol */
+	public final static int  MAXVALUE = 255;
+	/** Minimum output-value for DMX-protocol */
+	public final static int  MINVALUE = 0;
+
+	
+	//Effects constants
+	/** Range of the state of an effect. Loopover-point for it. */
+	public final static int EFFECTRANGE = 65536;
+	/** length of one interval for random effects */
+	public final static int EFFECTSTEP = 16384;
+	/** Time in Milliseconds between Effect-Ticks */
+	public final static int EFFECTTICKMILLIS = 25;
+}
--- src/c4l/server/database/information.java
+++ src/c4l/server/database/information.java
@@ -1,125 +1,132 @@
 package c4l.server.database;
+
 import c4l.server.database.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.json.*;
+import c4l.server.database.Constants;
 
 //import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
 //import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
-public class information {
-	
-	public static int adrresen = 512;
-	public static int geraete = 32;
-	public static String effectSice = "0";
-	public static String effectSpeed = "0";
-	public static String scenenID = "1"; // initzalisirungs wert fürs Hochfahren damit immer eine Scenen zurück gegebn wird
-	public static String caseID = "1";
-	public static String effectID = "0"; // Initzalisirt das kein button gedrückt ist
-	public static String addresValue[] = new String[adrresen+1]; // 512 adresse
-	
-	private static String geraeteValue[] = new String[geraete]; // 32 Geräte
-	private static Boolean geraeteUse[] = new Boolean[geraete]; 
-	 
-//	public String 
-	
-	public information(){
-		
-	}	
-	public static void setEffectID(String id){
+public final class information {
+
+	/*
+	 * TO DO :
+	 * 
+	 * Scenen und cases müsseListen werden ... Fader müssen einzeln übergeben werden
+	 * 
+	 * 
+	 */
+
+	private static byte effectSice = 0;
+	private static byte effectSpeed = 0;
+	private static ArrayList<Integer> scenenID = new ArrayList<>(); // damit mehrer scenen Paralel laufen könne
+	private static Integer caseID;
+	private static Integer effectID; // Initzalisirt das kein button gedrückt ist
+	// public static String addresValue[] = new String[adrresen+1]; // 512 adresse
+	private static byte faderValue[] = new byte[Constants.DEVICE_CHANNELS];
+	private static Boolean geraeteUse[] = new Boolean[Constants.DYNAMIC_DEVICES];
+
+	// public String
+
+	public information() {
+
+	}
+
+	public static void setEffectID(Integer id) {
 		effectID = id;
 	}
-	
-//	public static string test 
-	
-	public static void setScenenID(String scenenID){
-		information.scenenID = scenenID;
+
+	// public static string test
+
+	public static void addScenenID(Integer scenenID) {
+		information.scenenID.add(scenenID);
 	}
-	
-	public static void setSice(String sice){
+
+	public static void dropScenenID(Integer scenenID) {
+		information.scenenID.remove(scenenID);
+	}
+
+	public static void setSice(byte sice) {
 		information.effectSice = sice;
 	}
-	
-	public static void setSpeed(String speed){
+
+	public static void setSpeed(byte speed) {
 		information.effectSpeed = speed;
 	}
-	
-	public static void setGeraet(String Geraet){
-		try{
-		if(geraeteUse[Integer.valueOf(geraete)]){
-			geraeteUse[Integer.valueOf(geraete)] = false;
-		}else{
-			geraeteUse[Integer.valueOf(geraete)] = true;
-		}
-		}catch (Exception e) {
-			geraeteUse[Integer.valueOf(geraete)] = true;
+
+	public static void setGeraet(Integer Geraet) {
+		try {
+			if (geraeteUse[Geraet]) {
+				geraeteUse[Geraet] = false;
+			} else {
+				geraeteUse[Geraet] = true;
+			}
+		} catch (Exception e) {
+			geraeteUse[Geraet] = true;
 		}
 	}
-	
-	// weil es mit einer stelle zuviel übergeben wird
-	public static void setAdrresValue(String adress, String value){
-		addresValue[ Integer.parseInt(adress)-1] =  value;
+
+	public static void setFader(byte fader, byte value) {
+		information.faderValue[fader] = value;
+
 	}
-	
-	public static void getGeraeteUse(){
-		String gU ="";
-		for(int i=0 ; i <= geraeteUse.length; i++){
-			if(geraeteUse[i]){
-				gU = gU + i+";";
+
+	/*
+	 * // weil es mit einer stelle zuviel übergeben wird public static void
+	 * setAdrresValue(String adress, String value){ addresValue[
+	 * Integer.parseInt(adress)-1] = value; }
+	 */
+
+	public static String getGeraeteUse() {
+		String gU = "";
+		for (int i = 0; i <= geraeteUse.length; i++) {
+			if (geraeteUse[i]) {
+				gU = gU + i + ";";
 			}
 		}
+		return gU;
 	}
-	
-	public static String getEffectID(){
+
+	public static String getScenenIDs() {
+		String SIds = "";
+		Iterator<Integer> iterator = scenenID.iterator();
+		while (iterator.hasNext()) {
+			SIds = SIds + iterator.next() + ";";
+		}
+		return SIds;
+	}
+
+	public static Integer getEffectID() {
 		return information.effectID;
 	}
-	
-	public static String getScenenID(){
+
+	public static ArrayList<Integer> getScenenID() {
 		return information.scenenID;
 	}
-	
-	private static void buildGeraeteValues(){
-		int p = 0;
-		for ( int i = 0; i <= adrresen-16; i+=16 ) {
-			String values = "";
-			
-			for (int y = i; y <i+16;y++){
-				values = values+addresValue[y]+";";
-				if(i==0){
-					p = 0;
-				}else{
-					p = i/16;
-				}
-			}
-			geraeteValue[p] = values;
-		}
-			
+
+	public static JSONArray getFaderValues() {
+		JSONArray array = new JSONArray();
+		for (byte i : faderValue)
+			array.put(i, faderValue[i]);
+
+		return array;
+
 	}
-	
-	private static String[] clearAdrresValues(String[] addresValue){
-		System.out.println(addresValue[1]);
-		String a;
-		for(int i = 0; i<= addresValue.length;i++){
-			a = addresValue[i];
-			if(a == null){
-				addresValue[i]="0";
-			}
-		}
-		return addresValue;
-	}
-	
-	public static String getinfoJSON(){
-		//addresValue = clearAdrresValues(addresValue);
-		buildGeraeteValues();
+
+	public static String getinfoJSON() {
+		// addresValue = clearAdrresValues(addresValue);
 		JSONObject answer = new JSONObject();
-		answer.put("scenenID", scenenID);
-        answer.put("addresValue", addresValue);
-        answer.put("caseID", caseID);
-        answer.put("geraeteValue", geraeteValue);
-        answer.put("effectSize", effectSice);
-        answer.put("effectSpeed", effectSpeed);
+		answer.put("scenenID", getScenenIDs());
+		answer.put("caseID", caseID);
+		answer.put("effectSize", effectSice);
+		answer.put("effectSpeed", effectSpeed); // könnte noch ne Logi rein das nur geändert values rein kommen
+		answer.put("fader", getFaderValues());
 		return answer.toString();
 	}
 
