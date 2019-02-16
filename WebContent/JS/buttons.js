2a83b6b0885901e0d7f973bdc9c77648b30c52a6
Andre Soltau <Modellbauer555@web.de>
2019-02-16 18:04:26 +0100

    values -1

--- WebContent/JS/buttons.js
+++ WebContent/JS/buttons.js
@@ -2,84 +2,84 @@ $( function() {
 	
 	// Deklaratzion der Effect Button 
       $( "#effect1" ).click( function( event ) {
-    	  setEffect(1);
+    	  setEffect(0);
         } );
     
       $( "#effect2" ).click( function( event ) {
-    	  setEffect(2);
+    	  setEffect(1);
       } );
       
       $( "#effect3" ).click( function( event ) {
-    	  setEffect(3);
+    	  setEffect(2);
         } );
       
       $( "#effect4" ).click( function( event ) {
-    	  setEffect(4);
+    	  setEffect(3);
         } );
       
       $( "#effect5" ).click( function( event ) {
-    	  setEffect(5);
+    	  setEffect(4);
         } );
       
       $( "#effect6" ).click( function( event ) {
-    	  setEffect(6);
+    	  setEffect(5);
         } );
       
       $( "#effect7" ).click( function( event ) {
-    	  setEffect(7);
+    	  setEffect(6);
         } );
       
       $( "#effect8" ).click( function( event ) {
-    	  setEffect(8);
+    	  setEffect(7);
         } );
       
       $( "#effect9" ).click( function( event ) {
-    	  setEffect(9);
+    	  setEffect(8);
         } );
       
       $( "#effect10" ).click( function( event ) {
-    	  setEffect(10);
+    	  setEffect(9);
         } );
       
       
    // Deklaratzion der Geräte Button 
       $( "#g1" ).click( function( event ) {
-    	  setGeraet(1);
+    	  setGeraet(0);
         } );
     
       $( "#g2" ).click( function( event ) {
-    	  setGeraet(2);
+    	  setGeraet(1);
       } );
       
       $( "#g3" ).click( function( event ) {
-    	  setGeraet(3);
+    	  setGeraet(2);
         } );
       
       $( "#g4" ).click( function( event ) {
-    	  setGeraet(4);
+    	  setGeraet(3);
         } );
       
       $( "#g5" ).click( function( event ) {
-    	  setGeraet(5);
+    	  setGeraet(4);
         } );
       
       $( "#g6" ).click( function( event ) {
-    	  setGeraet(6);
+    	  setGeraet(5);
         } );
       
       $( "#g7" ).click( function( event ) {
-    	  setGeraet(7);
+    	  setGeraet(6);
         } );
       
       $( "#g8" ).click( function( event ) {
-    	  setGeraet(8);
+    	  setGeraet(7);
         } );
       $( "#g9").click( function( event ) {
-    	  setGeraet(9);
+    	  setGeraet(8);
         } );
       
       $( "#g10" ).click( function( event ) {
-    	  setGeraet(10);
+    	  setGeraet(9);
         } );
  
       function setEffect(effectNr){
